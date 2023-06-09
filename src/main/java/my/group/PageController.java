6edb296e5128a1b9c6afd22d816d819ea8719cd2
Zhyriutun Dmytro <dum4uk92@gmail.com>
2023-06-09 22:58:36 +0300

    first commit

---
+++ src/main/java/my/group/PageController.java
@@ -0,0 +1,13 @@
+package my.group;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String getInternationalPage() {
+        return "home";
+    }
+}
